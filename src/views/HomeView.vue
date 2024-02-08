629400ef34017d15c6daaaa772965bb313e549ab
Your Name <your_email@whatever.com>
2024-02-08 23:15:28 +0700

    Реализация отображения каталога

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div>
+    <header>
+      <article>
+        <img src="../assets/logo.png" alt="logo"/>
+      </article>
+      <nav>
+        <button @click="getData">Продукты</button>
+      </nav>
+    </header>
+  </div>
+  <div class="catalog" v-for="product in products" :key="product.id">
+    Название продукта: {{ product.name }} <br />
+    Описание: {{ product.description }} <br />
+    Цена:{{ product.price }}руб. <br />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
-
 export default {
   name: 'HomeView',
-  components: {
+  data() {
+    return {
+      products: [],
+    }
+  },
+  methods: {
+    async getData() {
+      const url = "https://jurapro.bhuser.ru/api-shop/products";
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        const result = await response.json();
+        this.products = result.data
+        console.log('Result: ', result)
+      } else {
+        this.error = "Ошибка";
+        console.error(this.error);
+      }
+    }
   }
 }
 </script>
+
+<style>
+.catalog{
+  border: 1px solid black;
+  margin: 12px;
+}
+</style>
