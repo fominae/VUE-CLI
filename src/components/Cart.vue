b4dc692c6063e290828693f2973b0259ed43d92a
Your Name <your_email@whatever.com>
2024-02-09 15:14:23 +0700

    Реализация удаления товара из корзины

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <h1>Корзина</h1>
-    <div v-for="product in products" :key="product.id">
+    <div class="catalog" v-for="product in products" :key="product.id">
       Название продукта: {{ product.name }} <br/>
       Описание: {{ product.description }} <br/>
       Цена:{{ product.price }}руб. <br/>
+      Количество: {{ product.quantity }} <br/>
+      <button @click="increaseQuantity(product)">+</button>
+      <button @click="decreaseQuantity(product)">-</button>
+      <button @click="removeFromCart(product)">Удалить из корзины</button>
     </div>
   </div>
 </template>
@@ -40,8 +44,38 @@ export default {
         console.error(this.error);
       }
     },
-  }
-};
+    async removeFromCart(product) {
+      const localToken = localStorage.getItem('token');
+      if (!localToken) {
+        console.error('Токен пользователя отсутствует.');
+        return;
+      }
+
+      try {
+        const response = await fetch(`${this.url}/cart/${product.id}`, {
+          method: "DELETE",
+          headers: {
+            'Authorization': `Bearer ${localToken}`
+          }
+        });
+        if (response.ok) {
+          console.log("Товар успешно удален из корзины");
+          location. reload()
+
+        } else {
+          console.error("Ошибка удаления товара из корзины:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Ошибка удаления товара из корзины:", error);
+      }
+    },
+  },
+}
 </script>
 
-
+<style>
+.catalog {
+  border: 1px solid black;
+  margin: 12px;
+}
+</style>
